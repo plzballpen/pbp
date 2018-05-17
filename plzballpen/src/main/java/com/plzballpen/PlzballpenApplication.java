48af89f88ddf0643dafc0092606e0a3dc7a30300
plzballpen <plzballpen@gmail.com>
2018-05-17 17:00:33 +0900

    init2

--- plzballpen/src/main/java/com/plzballpen/PlzballpenApplication.java
+++ plzballpen/src/main/java/com/plzballpen/PlzballpenApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlzballpenApplication {
 
+	//링크 = http://private.tistory.com/36
+	//주소 = http://localhost:8080/board
 	public static void main(String[] args) {
 		SpringApplication.run(PlzballpenApplication.class, args);
 	}
