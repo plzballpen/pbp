36fb30c1c3f425486fce9ddb31ebbf0ce03e2c87
plzballpen <plzballpen@gmail.com>
2018-05-20 19:07:31 +0900

    maven basic 1

---
+++ homepage/src/main/java/com/homepage/HomepageApplication.java
@@ -0,0 +1,12 @@
+package com.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageApplication {
+//http://private.tistory.com/51?category=753861
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageApplication.class, args);
+	}
+}
---
+++ homepage/src/main/java/com/homepage/JspTest.java
@@ -0,0 +1,12 @@
+package com.homepage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JspTest {
+	@RequestMapping("/test")
+	private String jspTest(){
+		return "test"; //생성한 jsp
+	}
+}
